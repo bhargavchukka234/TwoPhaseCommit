da595eb3335712c6a99be89eb67bc8bbf88e20e7
bhargavchukka234 <bhargavchukka234@gmail.com>
2020-03-06 14:52:58 -0800

    deleted sender

--- communication_utils.py
+++ communication_utils.py
@@ -1,12 +1,15 @@
 import json
 from constants import State
 
-def sendMessageToCohort(channel, processNumber, state, insert_statements_list):
+def sendMessageToCohort(channel, processNumber, state, insert_statements_list = None):
     # TODO: remove the hardcoded value for the queue1 and pass the actual queue name for the function
     queueName = "queue" + str(processNumber)
     messageBody = ""
     if (state == State.PREPARE):
         count = 0
+        if (insert_statements_list == None):
+            return
+
         for line in insert_statements_list:
             if (count <= 10):
                 if (line.__contains__("INSERT")):
