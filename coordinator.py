62e5305f8633929829576c6bd31ce98ed72c0d3e
bhargavchukka234 <bhargavchukka234@gmail.com>
2020-03-05 16:29:28 -0800

    coordinator changes

--- coordinator.py
+++ coordinator.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class TransactionState(Enum):
     NOT_STARTED = 1
     INITIATED = 2
@@ -8,64 +9,65 @@ class TransactionState(Enum):
     ABORTED = 5
     COMPLETED = 6
 
+
 class Transaction:
 
     def __init__(self, id):
         self.id = id
         self.ack_table = dict()
         self.prepared_table = dict()
-        self.state = NOT_STARTED
+        self.state = TransactionState.NOT_STARTED
         self.cohorts = []
 
-    def set_ack_received(cohort):
-        if cohort in cohorts:
-            ack_table[cohort] = True
-        
-    def set_cohort_prepared_status(cohort, cohort_status):
-        if cohort in cohorts:
-            prepared_table[cohort] = cohort_status
+    def set_ack_received(self, cohort):
+        if cohort in self.cohorts:
+            self.ack_table[cohort] = True
 
-    def set_transaction_state(state):
+    def set_cohort_prepared_status(self, cohort, cohort_status):
+        if cohort in self.cohorts:
+            self.prepared_table[cohort] = cohort_status
+
+    def set_transaction_state(self, state):
         self.state = state
 
-    def set_cohort_list(cohorts):
+    def set_cohort_list(self, cohorts):
         self.cohorts = cohorts
 
-    def check_all_cohorts_prepared():
-        return len(ack_table) == len(cohorts)
+    def check_all_cohorts_prepared(self):
+        return len(self.ack_table) == len(self.cohorts)
 
-    def check_cohorts_acked():
-        return len(ack_table) == len(cohorts)
+    def check_all_cohorts_acked(self):
+        return len(self.ack_table) == len(self.cohorts)
 
-    
+
 class ProtocolDB:
 
     def __init__(self):
         self.transactions = dict()
 
-    def add_transaction(transaction, cohorts):
+    def add_transaction(self, transaction, cohorts):
         self.transactions[transaction.id] = transaction
-        self.transactions[transaction.id].set_cohort_list
+        self.transactions[transaction.id].set_cohort_list(cohorts)
 
-    def remove_transaction(transaction_id):
-        del self.transactions[transaction.id]
+    def remove_transaction(self, transaction_id):
+        del self.transactions[transaction_id]
 
-    def set_ack_received(transaction_id, cohort):
-        self.transactions[transaction.id].set_ack_received(cohort)
+    def set_ack_received(self, transaction_id, cohort):
+        self.transactions[transaction_id].set_ack_received(cohort)
 
-    def set_cohort_prepared_status(transaction_id, cohort, cohort_status):
-        self.transactions[transaction.id].set_cohort_prepared_status(cohort, cohort_status)
+    def set_cohort_prepared_status(self, transaction_id, cohort, cohort_status):
+        self.transactions[transaction_id].set_cohort_prepared_status(cohort, cohort_status)
 
-    def set_transaction_state(transaction_id, state):
-        self.transactions[transaction.id].set_transaction_state(state)
+    def set_transaction_state(self, transaction_id, state):
+        self.transactions[transaction_id].set_transaction_state(state)
 
-    def check_all_cohorts_prepared(transaction_id):
-        return self.transactions[transaction.id].check_all_cohorts_prepared()
+    def check_all_cohorts_prepared(self, transaction_id):
+        return self.transactions[transaction_id].check_all_cohorts_prepared()
 
-    def check_all_cohorts_acked(transaction_id):
-        return self.transactions[transaction.id].check_all_cohorts_acked()
-        
-    
+    def check_all_cohorts_acked(self, transaction_id):
+        return self.transactions[transaction_id].check_all_cohorts_acked()
+
+
 class Coordinator:
     """
     Coordinator for a 2 Phase Commit
@@ -78,10 +80,8 @@ class Coordinator:
         self.log_file = log_file
         self.protocol_DB = ProtocolDB()
 
-    def start():
+    def start(self):
         self.recover_from_logs()
 
-    def run():
+    def run(self):
         self.construct_transaction()
-
-
