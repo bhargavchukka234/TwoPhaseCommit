b060789bdb91e833da47e170c3c1767e3807ddff
Akshay V Bhandiwad <akshayvb23>
2020-03-06 15:00:20 -0800

    Added the handling of protocolDB/timeout to cohort response function

--- coordinator.py
+++ coordinator.py
@@ -17,7 +17,7 @@ class Transaction:
         if cohort in self.cohorts:
             self.ack_table[cohort] = True
 
-    def set_cohort_prepared_status(self, cohort, cohort_status):
+    def set_cohort_decision(self, cohort, cohort_status):
         if cohort in self.cohorts:
             self.prepared_table[cohort] = cohort_status
 
@@ -27,11 +27,17 @@ class Transaction:
     def set_cohort_list(self, cohorts):
         self.cohorts = cohorts
 
-    def check_all_cohorts_prepared(self):
-        return len(self.ack_table) == len(self.cohorts)
+    def check_all_cohorts_responded_to_prepare(self):
+        return len(self.prepared_table) == len(self.cohorts)
 
     def check_all_cohorts_acked(self):
         return len(self.ack_table) == len(self.cohorts)
+
+    def get_prepared_decision(self):
+        for cohort, decision in self.prepared_table:
+            if decision == State.ABORT:
+                return State.ABORT
+        return State.COMMIT
 
 
 class ProtocolDB:
@@ -49,17 +55,20 @@ class ProtocolDB:
     def set_ack_received(self, transaction_id, cohort):
         self.transactions[transaction_id].set_ack_received(cohort)
 
-    def set_cohort_prepared_status(self, transaction_id, cohort, cohort_status):
-        self.transactions[transaction_id].set_cohort_prepared_status(cohort, cohort_status)
+    def set_cohort_decision(self, transaction_id, cohort, cohort_status):
+        self.transactions[transaction_id].set_cohort_decision(cohort, cohort_status)
 
     def set_transaction_state(self, transaction_id, state):
         self.transactions[transaction_id].set_transaction_state(state)
 
-    def check_all_cohorts_prepared(self, transaction_id):
-        return self.transactions[transaction_id].check_all_cohorts_prepared()
+    def check_all_cohorts_responded_to_prepare(self, transaction_id):
+        return self.transactions[transaction_id].check_all_cohorts_responded_to_prepare()
 
     def check_all_cohorts_acked(self, transaction_id):
         return self.transactions[transaction_id].check_all_cohorts_acked()
+
+    def get_prepared_decision(self, transaction_id):
+        return self.transactions[transaction_id].get_prepared_decision()
 
     def empty(self):
         return len(self.transactions) == 0
@@ -84,11 +93,43 @@ class Coordinator:
         # start the recovery thread
         self.recovery_thread.start()
 
+    def cohortResponse(self, channel, method, properties, body):
+        print(" [x] Received response from cohort %r" % body)
+
+        #the coordinator proceeds with sending the next message after receiving a message from receiver
+        dict_obj = json.loads(body)
+        new_obj = dict_obj.get('message')
+        state = new_obj.get('state')
+        transaction_id = new_obj.get('id')
+        cohort = new_obj.get('cohort')
+
+        if(state == State.PREPARED or state == State.ABORT):
+            # mark the receipt of this PREPARED message in the protocol DB for the particular cohort
+            self.protocol_DB.set_cohort_decision(transaction_id, cohort, state)
+            # check if all cohorts have responded to prepare
+            if self.protocol_DB.check_all_cohorts_responded_to_prepare(transaction_id):
+                # send COMMIT/ABORT depending on the final decision of the cohorts
+                sendMessageToCohort(channel, cohort, self.protocol_DB.get_prepared_decision(transaction_id))
+                # add this transaction to the timer monitor list for recovery 
+                self.timeout_transaction_info[transaction_id] = COMMIT_ACK_TIMEOUT
+
+        elif(state == State.ACK):
+            # mark the receipt of this ACK message in the protocol DB for the particular cohort
+            self.protocol_DB.set_ack_received(transaction_id, cohort, state)
+            # check if we received acks from all the cohorts
+            if self.protocolDB.check_all_acks_received(transaction_id):
+                # Remove the transaction from the protocol DB
+                self.protocolDB.remove_transaction(transaction_id)
+                # Remove the transaction from the timer monitor list
+                if transaction_id in self.timeout_transaction_info.keys():
+                    del self.timeout_transaction_info[transaction_id]
+
+
     def run(self):
         # required if coordinator comes up after crash/failure
         if not self.protocol_DB.empty():
             for transaction in self.protocol_DB.transactions:
-                self.timeout_transaction_info[transaction.id, COMMIT_ACK_TIMEOUT]
+                self.timeout_transaction_info[transaction.id] = COMMIT_ACK_TIMEOUT
                 for cohort in transaction.cohorts:
                     sendMessageToCohort(self.channel, cohort, State.COMMIT)
 
@@ -106,24 +147,8 @@ class Coordinator:
         # time.sleep(2)
         self.channel.basic_consume(queue='coordinatorQueue',
                                    auto_ack=True,
-                                   on_message_callback=cohortResponse)
+                                   on_message_callback=self.cohortResponse)
         self.channel.start_consuming()
-
-
-def cohortResponse(channel, method, properties, body):
-    print(" [x] Received response from cohort %r" % body)
-    #the coordinator proceeds with sending the next message after receiving a message from receiver
-    dict_obj = json.loads(body)
-    new_obj = dict_obj.get('message')
-    state = new_obj.get('state')
-    if(state == State.PREPARED):
-        # receive prepared from all the cohorts
-        sendMessageToCohort(channel, 1, State.COMMIT)
-    elif(state == State.ACK):
-        '''TODO: delete from protocol DB'''
-    elif(state == State.ABORT):
-        # send messages to all the
-        sendMessageToCohort(channel, 1, State.ABORT)
 
 
 if __name__ == "__main__":
