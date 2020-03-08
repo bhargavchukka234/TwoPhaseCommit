f3a6c358eca63883464286fbb42ddb19362e5e54
bhargavchukka234 <bhargavchukka234@gmail.com>
2020-03-07 16:02:02 -0800

    commendted db operations

--- transaction_log_utils.py
+++ transaction_log_utils.py
@@ -5,17 +5,19 @@ import json
 from transaction import Transaction
 
 # create a connection the database
-dbConnection = psycopg2.connect(database="test", user="newuser", password="password", host="127.0.0.1",
-                                port="5431")
-cursor = dbConnection.cursor()
+# dbConnection = psycopg2.connect(database="test", user="newuser", password="password", host="127.0.0.1",
+#                    port="5431")
+# cursor = dbConnection.cursor()
 
 
 def insert_log(self, transaction):
-    self.cursor.execute(INSERT_SQL, (transaction.id, int(transaction.state)), json.dumps(transaction.cohorts))
+    # cself.cursor.execute(INSERT_SQL, (transaction.id, int(transaction.state)), json.dumps(transaction.cohorts))
+    pass
 
 
 def delete_log(self, transaction_id):
-    self.cursor.execute(DELETE_SQL, transaction_id)
+    # self.cursor.execute(DELETE_SQL, transaction_id)
+    pass
 
 
 def get_pending_transactions(self):
